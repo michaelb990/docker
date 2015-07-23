be05d32525ae92e6d5ade886626edd9dcff2f916
Michael Brown <michael.brown@socrata.com>
2015-07-23 22:34:47 +0000

    Update build to use a docker-init folder and start working on gitconfig

--- lucid/docker-init/rbenv-install.sh
+++ lucid/docker-init/rbenv-install.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
+# Install rbenv and ruby version 1.9.3-p484
+
 export HOME=/home/jenkins
+
 git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
 git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.profile
 echo 'eval "$(rbenv init -)"' >> ~/.profile
 source ~/.profile
+
 rbenv install 1.9.3-p484
 rbenv global 1.9.3-p484
+
+gem install bundler
+
+rbenv rehash
