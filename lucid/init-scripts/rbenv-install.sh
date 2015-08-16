4ee9c2ff8feebfd9d2b816398bf4c5628b5cd38d
Michael Brown <michael.brown@socrata.com>
2015-08-16 02:01:49 +0000

    improvements to Dockerfile & rbenv install script

--- lucid/init-scripts/rbenv-install.sh
+++ lucid/init-scripts/rbenv-install.sh
@@ -4,16 +4,18 @@
 
 export HOME=/home/jenkins
 
-git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
-git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+echo "Cloning rbenv and ruby-build into jenkins home directory."
+git clone --depth 1 https://github.com/sstephenson/rbenv.git ~/.rbenv
+git clone --depth 1 https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
 
+echo "Setting up profile to use rbenv."
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.profile
 echo 'eval "$(rbenv init -)"' >> ~/.profile
 source ~/.profile
 
-rbenv install 1.9.3-p484
-rbenv global 1.9.3-p484
-
+ruby_version=1.9.3-p484
+echo "Installing ruby $ruby_version." 
+rbenv install $ruby_version
+rbenv global $ruby_version
 gem install bundler
-
 rbenv rehash
